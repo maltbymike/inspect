d040f4d0833601baf46e7bd89238640b0f471f59
Mike Maltby <mike@ingersollrentall.ca>
2024-05-23 21:41:16 -0400

    Create Item livewire component

---
+++ resources/views/livewire/items/item.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <x-slot name="header">
+        <x-page-title>
+            {{ __('Items') }}
+        </x-page-title>
+    </x-slot>
+    
+    <x-main-content-box>
+        <x-content-box-section>
+            Test
+        </x-content-box-section>
+    </x-main-content-box>
+</div>
