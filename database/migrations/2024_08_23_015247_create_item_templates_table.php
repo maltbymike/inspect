941390bbed75a731a2c83a283653a5f404e4e202
Mike Maltby <mike@ingersollrentall.ca>
2024-08-22 23:10:22 -0400

    Create migrations to support item inspections

---
+++ database/migrations/2024_08_23_015247_create_item_templates_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Items\Inspections\Template;
+use App\Models\Items\Item;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_template', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id');
+            $table->foreign('item_id')
+                ->references('id')
+                ->on('items');
+            $table->foreignId('template_id');
+            $table->foreign('template_id')
+                ->references('id')
+                ->on('item_inspection_templates');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_template');
+    }
+};
---
+++ database/migrations/2024_08_23_023154_create_item_inspections_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Models\Items\Inspections\ItemTemplate;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_inspections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_template_id');
+            $table->foreign('item_template_id')
+                ->references('id')
+                ->on('item_template');
+            $table->foreignId('assigned_to_user_id')
+                ->nullable();
+            $table->foreign('assigned_to_user_id')
+                ->references('id')
+                ->on('users');
+            $table->foreignId('completed_by_user_id')
+                ->nullable();
+            $table->foreign('completed_by_user_id')
+                ->references('id')
+                ->on('users');
+            $table->foreignId('approved_by_user_id')
+                ->nullable();
+            $table->foreign('approved_by_user_id')
+                ->references('id')
+                ->on('users');
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_inspections');
+    }
+};
