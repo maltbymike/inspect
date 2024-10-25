ed6bcf7a5d09550319f8e31270fe75c8395fd7d0
Mike Maltby <mike@ingersollrentall.ca>
2024-10-24 23:09:35 -0400

    Add soft deletes to Items Model

--- app/Models/Items/Item.php
+++ app/Models/Items/Item.php
@@ -9,12 +9,14 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Staudenmeir\LaravelAdjacencyList\Eloquent\HasGraphRelationships;
 
 class Item extends Model
 {
     use HasGraphRelationships;
     use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = [];
 
