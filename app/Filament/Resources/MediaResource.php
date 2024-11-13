c0709dd02b219a1b9b86c322959ebe053d1a4d1b
Mike Maltby <mike@ingersollrentall.ca>
2024-11-12 20:00:26 -0500

    Add EditHistory to MediaResource

--- app/Filament/Resources/MediaResource.php
+++ app/Filament/Resources/MediaResource.php
@@ -2,22 +2,48 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\MediaResource\Pages;
-use App\Filament\Resources\MediaResource\RelationManagers;
-use App\Models\Media;
-use BezhanSalleh\FilamentShield\Contracts\HasShieldPermissions;
-use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Traits\HasStandardTableActions;
+use App\Filament\Resources\MediaResource\Pages;
+use BezhanSalleh\FilamentShield\Contracts\HasShieldPermissions;
+use Awcodes\Curator\Resources\MediaResource as CuratorMediaResource;
 
-class MediaResource extends \Awcodes\Curator\Resources\MediaResource implements HasShieldPermissions
+class MediaResource extends CuratorMediaResource implements HasShieldPermissions
 {
+    use HasStandardTableActions;
+    
+    public static function getModel(): string
+    {
+        return config('curator.model');
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => CuratorMediaResource\ListMedia::route('/'),
+            'create' => CuratorMediaResource\CreateMedia::route('/create'),
+            'edit' => CuratorMediaResource\EditMedia::route('/{record}/edit'),
+            'edit-history' => Pages\EditHistory::route('/{record}/edit/history'),
+        ];
+    }
+
     public static function getPermissionPrefixes(): array
     {
         return config('filament-shield.permission_prefixes.resource');
     }
+
+    public static function table(Table $table): Table
+    {
+        return parent::table($table)
+            ->actions([
+                Tables\Actions\ActionGroup::make(array_merge(
+                    Static::StandardTableActions(),
+                    [
+                        Tables\Actions\DeleteAction::make(),
+                    ],
+                )),
+            ]);
+    }
+
 }
---
+++ app/Filament/Resources/MediaResource/Pages/EditHistory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\MediaResource\Pages;
+
+use App\Filament\Resources\MediaResource;
+use pxlrbt\FilamentActivityLog\Pages\ListActivities;
+
+class EditHistory extends ListActivities
+{
+    protected static string $resource = MediaResource::class;
+}
