65ba1a39a6b1393441d33bcc05548505901be8b1
Mike Maltby <mike@ingersollrentall.ca>
2024-11-17 08:58:26 -0500

    Group timestamps and user fields on ItemInspectionResource form

--- app/Filament/Resources/ItemInspectionResource.php
+++ app/Filament/Resources/ItemInspectionResource.php
@@ -37,11 +37,23 @@ class ItemInspectionResource extends Resource implements HasShieldPermissions
                 ->columns(2)
                 ->collapsed()
                 ->schema([
-                    Forms\Components\DateTimePicker::make('created_at'),
-                    Forms\Components\DateTimePicker::make('started_at'),
-                    Forms\Components\DateTimePicker::make('completed_at'),
-                    Forms\Components\Select::make('completed_by_user_id')
-                        ->relationship('completedByUser', 'name'),
+                    Forms\Components\Fieldset::make('Timestamps')
+                        ->columnSpan(1)
+                        ->schema([
+                            Forms\Components\DateTimePicker::make('created_at'),
+                            Forms\Components\DateTimePicker::make('started_at'),
+                            Forms\Components\DateTimePicker::make('completed_at'),
+                        ]),
+                    Forms\Components\Fieldset::make('Responsible Users')
+                        ->columnSpan(1)
+                        ->schema([
+                        Forms\Components\Select::make('assigned_to_user_id')
+                            ->relationship('assignedToUser', 'name'),
+                        Forms\Components\Select::make('completed_by_user_id')
+                            ->relationship('completedByUser', 'name'),
+                        Forms\Components\Select::make('approved_by_user_id')
+                            ->relationship('approvedByUser', 'name'),
+                        ]),
                 ]),
             Forms\Components\Actions::make([
                 Forms\Components\Actions\Action::make('startInspection')
