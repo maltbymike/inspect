b20282b7a0419912b540c1c65c6ff1aad2e75c06
Mike Maltby <mike@ingersollrentall.ca>
2024-11-18 22:39:22 -0500

    Add getTabs to ListItemInspections to separate inspections by status

--- app/Filament/Resources/ItemInspectionResource/Pages/ListItemInspections.php
+++ app/Filament/Resources/ItemInspectionResource/Pages/ListItemInspections.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\ItemInspectionResource\Pages;
 
 use App\Filament\Resources\ItemInspectionResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListItemInspections extends ListRecords
 {
@@ -14,6 +16,25 @@ class ListItemInspections extends ListRecords
     {
         return [
             // Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'Pending' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->where('started_at', null)
+                ),
+            'In Process' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->where('completed_at', null)
+                    ->where('started_at', '!=', null)
+                ),
+            'Completed' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->where('completed_at', '!=', null)
+                ),
         ];
     }
 }
